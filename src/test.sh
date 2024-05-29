ed7b7114ecf2079adb6bb34ffead605ca232e49f
cuong.domanh <cuong.domanh@vti.com.vn>
2024-05-29 17:56:22 +0700

    fix add newline when fill data to file csv

--- src/test.sh
+++ src/test.sh
@@ -8,36 +8,24 @@ echo "store_code,type,year,january,february,march,april,may,june,july,august,sep
 
 csv_files=$(ls $(dirname "$0")/../master-data/monthly-holiday/)
 current_year=$(date +'%Y')
+created_at=$(date +'%Y-%m-%d')
 
 for csv_file in $csv_files; do
   echo "Processing file: $csv_file"
   store_code=$(echo "$csv_file" | sed 's/_.*//')
   type=1
-  while IFS=',' read -r col1 january february march april may june july august september october november december
+  while IFS=',' read -r label january february march april may june july august september october november december
   do
-     store_code="$store_code"
-     type="$type"
-     year="$current_year"
-     january="$january"
-     february="$february"
-     march="$march"
-     april="$april"
-     may="$may"
-     june="$june"
-     july="$july"
-     august="$august"
-     september="$september"
-     october="$october"
-     november="$november"
-     december="$december"
-     created_at='2024-05-29'
-     updated_at='2024-05-29'
-     echo "$store_code,$type,$year,$january,$february,$march,$april,$may,$june,$july,$august,$september,$october,$november,$december,$created_at,$updated_at">>"$(dirname "$0")/../output/monthly-holiday/test.csv"
+    DATA="$store_code,$type,$current_year,$january,$february,$march,$april,$may,$june,$july,$august,$september,$october,$november,$december"
+    ITEM1="${DATA},$created_at,$created_at"
+    echo ${ITEM1} | tr -d '\r' >>"$(dirname "$0")/../output/monthly-holiday/test.csv"
   ((type++)) 
   done < <(tail -n +2 "$(dirname "$0")/../master-data/monthly-holiday/$csv_file")
 done
+# wait
 
-
+# ITEM1="${ITEM},123,456"
+# echo ${ITEM1} | tr -d '\r'
 
 psql -h "${database_host}" -p "${database_port}" -d "${database_name}" -U "${database_user}" -c "\COPY $TABLE_NAME FROM '$(dirname "$0")/../output/monthly-holiday/test.csv' delimiter ',' csv header;"
 
