26664807c4c7ec173fad57f2445d028450e796b1
cuongdm250701 <domanhcuong250701@gmail.com>
2024-06-06 02:33:32 +0700

    demo sync data public holiday

--- src/public-holiday-81.sh
+++ src/public-holiday-81.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source $(dirname "$0")/../connect-db.sh
-connection_db
+# source $(dirname "$0")/../connect-db.sh
+# connection_db
 
 getDate() {
   local year=$1
@@ -24,7 +24,8 @@ month=10
 target_day=1
 target_week=2
 
-getDate $year $month $target_day $target_week
+get_date=$(getDate $year $month $target_day $target_week)
+echo "$get_date"
 
 
 
@@ -57,3 +58,48 @@ getDate $year $month $target_day $target_week
 # *)
 # echo "default case";;
 # esac
+
+### DOING ###
+### Type holiday ###
+TYPE_HOLIDAY_1='祝日'
+TYPE_HOLIDAY_2='行事'
+### Name table ###
+TABLE_NAME='public_holiday'
+### List columns in database's table ###
+columns="id,type,date,month,year,event_details,holiday,store_code,created_at,updated_at"
+### Create file contain store's list data monthly ### 
+path="$(dirname "$0")/../output/public-holiday/test.csv"
+echo "${columns}" > "${path}"
+### Handle get data ###
+csv_files=$(ls $(dirname "$0")/../master-data/public-holiday/)
+for csv_file in $csv_files; do
+  echo "Processing file: $csv_file"
+  store_code=$(echo "$csv_file" | sed 's/_.*//')
+  # echo "$store_code"
+  while IFS=',' read -r type_holiday month_holiday date_holiday event_details_holiday
+  do
+      # GET "$type_holiday"
+      # if [ $type_holiday = $TYPE_HOLIDAY_1 ]; then
+      #   echo "1"
+      # elif [ $type_holiday = $TYPE_HOLIDAY_2 ]; then
+      #   echo "2"
+      # fi
+      # GET MONTH
+       month=$(echo "$month_holiday" | sed 's/月//')
+      #  echo "$month"
+      # GET DATE
+      case "${date_holiday}_${month_holiday}" in 
+        "第２月曜日_１月") echo "thứ 2 tuần thứ 2 tháng 1";;
+        "第２日曜日_５月") echo "chủ nhật tuần thứ 2 tháng 5";;
+        "第３日曜日_６月") echo "chủ nhật tuần thứ 3 tháng 6";;
+        "第３月曜日_７月") echo "thứ 2 tuần thứ 3 tháng 7";;
+        "第３月曜日_９月") echo "thứ 2 tuần thứ 3 tháng 9";;
+        "第２月曜日_１０月") echo "thứ 2 tuần thứ 2 tháng 10";;
+        *)
+        echo "$date_holiday" | sed 's/日//';;
+      esac
+  done < <(iconv -f SHIFT-JIS -t UTF-8 "$(dirname "$0")/../master-data/public-holiday/$csv_file" | tail -n +2)
+done
+
+
+## Hoir laij case ngay va thang o file 283
